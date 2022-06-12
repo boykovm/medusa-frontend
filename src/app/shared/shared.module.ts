6f7133941115b97c50f073a25d332e2935d13fbf
boyk0 <v_boiko-itssv@kw.com>
2022-06-12 21:00:42 +0300

    create footer layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,6 +8,8 @@ import { LogoComponent } from './logo/logo.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { LanguageSwitcherComponent } from './language-switcher/language-switcher.component';
 import { ProfileButtonComponent } from './profile-button/profile-button.component';
+import { SiteMapComponent } from './site-map/site-map.component';
+import { FooterInfoComponent } from './footer-info/footer-info.component';
 
 
 
@@ -19,7 +21,9 @@ import { ProfileButtonComponent } from './profile-button/profile-button.componen
     LogoComponent,
     NavigationComponent,
     LanguageSwitcherComponent,
-    ProfileButtonComponent
+    ProfileButtonComponent,
+    SiteMapComponent,
+    FooterInfoComponent
   ],
   imports: [
     CommonModule,
